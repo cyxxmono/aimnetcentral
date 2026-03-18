e897104a585855500ef26670f05cb2111881b2ae
chenyxx <chenyxx@stu.xmu.edu.cn>
2026-03-18 09:55:13 +0800

    expose the metadata calss

--- aimnet/models/__init__.py
+++ aimnet/models/__init__.py
@@ -1,5 +1,5 @@
 from .aimnet2 import AIMNet2  # noqa: F401
-from .base import AIMNet2Base, load_model  # noqa: F401
+from .base import AIMNet2Base, load_model, ModelMetadata  # noqa: F401
 from .utils import (  # noqa: F401
     extract_coulomb_rc,
     extract_d3_params,
